72212e4d58268a978f173f91b122534033725237
Mary Cotter <marebearc@gmail.com>
2018-07-27 19:11:38 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,11 @@
 # person.rb
+class Person 
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+    
+end
+
+alan_kay = Person.new("alan_kay")
+adele_goldberg = Person.new("adele_goldberg")
