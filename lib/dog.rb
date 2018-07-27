b2ddd28b9533db33ed9db0b2b8448415c0e65e15
Mary Cotter <marebearc@gmail.com>
2018-07-27 19:11:21 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,12 @@
 # dog.rb
+class Dog 
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+    
+end
+
+fido = Dog.new("fido")
+snoopy = Dog.new("snoopy")
+lassie = Dog.new("lassie")
