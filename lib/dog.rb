1b59c4b77a648731e641694621ecef495f3c4d4d
Mary Cotter <marebearc@gmail.com>
2018-07-27 19:19:12 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,13 @@
 # dog.rb
+class Dog 
+  def bark
+    puts "Woof!"
+  end
+  def sit 
+    puts "The Dog is sitting"
+  end
+end
+
+# fido = Dog.new("fido")
+# snoopy = Dog.new("snoopy")
+# lassie = Dog.new("lassie")
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,12 @@
 # person.rb
+class Person 
+  def talk
+    puts "Hello World!"
+  end 
+  def walk
+    puts "The Person is walking"
+  end
+end
+
+# alan_kay = Person.new("alan_kay")
+# adele_goldberg = Person.new("adele_goldberg")
